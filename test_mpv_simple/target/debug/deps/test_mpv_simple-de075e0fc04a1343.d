91a326b95569bca883d5cea92c1bf7be100d29b1
Developer <dev@example.com>
2026-03-12 13:44:27 +0800

    Remove test_mpv_simple/target

--- test_mpv_simple/target/debug/deps/test_mpv_simple-de075e0fc04a1343.d
+++
@@ -1,5 +0,0 @@
-F:\ai-mplayer12\test_mpv_simple\target\debug\deps\test_mpv_simple-de075e0fc04a1343.d: src\main.rs
-
-F:\ai-mplayer12\test_mpv_simple\target\debug\deps\libtest_mpv_simple-de075e0fc04a1343.rmeta: src\main.rs
-
-src\main.rs:
--- test_mpv_simple/target/debug/test_mpv_simple.d
+++
@@ -1,1 +0,0 @@
-F:\ai-mplayer12\test_mpv_simple\target\debug\test_mpv_simple.exe: F:\ai-mplayer12\test_mpv_simple\src\main.rs
